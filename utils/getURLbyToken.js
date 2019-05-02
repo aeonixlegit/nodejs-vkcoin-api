eab6bff59215e338866a7fc77df8acc13dbb2cb5
Stepan Kuznetsov <cursedsealwinter@gmail.com>
2019-05-02 16:32:37 +0300

    — 1.1.3
    — Окончательный фикс получения ссылки.

--- utils/getURLbyToken.js
+++ utils/getURLbyToken.js
@@ -1,13 +1,13 @@
 const request = require('./request')
 
 module.exports = async (token) => {
-  const result = await request(`https://api.vk.com/method/apps.get?access_token=${token}&app_id=6915965&v=5.95`)
+  let result = await request(`https://api.vk.com/method/apps.get?access_token=${token}&app_id=6915965&v=5.95`)
 
-  if (result.error) {
+  if (result && !result.error) {
+    result = JSON.parse(result)
+    const { mobile_iframe_url } = result.response.items[0]
+    return mobile_iframe_url
+  } else {
     throw new Error('Не удалось получить ссылку на приложение. Попробуйте переполучить токен.')
   }
-
-  const { mobile_iframe_url } = result.response.items[0]
-
-  return mobile_iframe_url
 }
